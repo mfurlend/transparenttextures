842117a322e3da6f0c917156b18a96508774f6be
Mike Furlender <mfurlend@gmail.com>
2016-06-17 15:52:27 -0400

    added diamond

---
+++ extension/background.js
@@ -0,0 +1,48 @@
+var raw_url  = 'https://raw.github.com/mfurlend/transparenttextures/master/',
+    patterns = [],
+    page     = 0,
+    selector,
+    selected;
+
+chrome.extension.onMessage.addListener(function(message, info, cb) {
+    if (message == 'getPatterns') {
+        cb({
+            patterns: patterns,
+            page:     page,
+            selector: selector,
+            selected: selected
+        });
+    }
+    
+    else if (message.clear) {
+        selector = message.selector||'body';
+        chrome.tabs.insertCSS({
+                code: selector + ' { background-image:none !important; }'
+        });
+    }
+
+    else if (message.pattern) {
+        selector = message.selector||'body';
+        selected = message.pattern;
+        chrome.tabs.insertCSS({
+                code: selector + ' { background-image: url(' + raw_url + message.pattern + ') !important; }'
+        });
+    }
+    else if (message.page) {
+        page = message.page;
+    }
+});
+
+function loadPatterns(j) {
+    var file;
+    for (var i in j.data) {
+        file = j.data[i];
+        if (/\.png$/.test(file.name)) {
+            patterns.push(file);
+        }
+    }
+}
+
+var patternsJSON = document.createElement('script');
+patternsJSON.src = 'https://api.github.com/repos/mfurlend/transparenttextures/contents/?callback=loadPatterns'
+document.body.appendChild(patternsJSON);
---
+++ extension/popup.js
@@ -0,0 +1,100 @@
+var raw_url = 'https://raw.github.com/mfurlend/transparenttextures/master/',
+    patterns_div = document.getElementById('patterns'),
+    name_div     = document.getElementById('name'),
+    next_btn     = document.getElementById('next'),
+    clear_btn    = document.getElementById('clear'),
+    prev_btn     = document.getElementById('prev'),
+    target_div   = document.getElementById('target'),
+    patterns,
+    page,
+    selected,
+    selectedName,
+    pageMax;
+
+function loadPatterns() {
+
+    var pattern, 
+        file,
+        start = page * 12,
+        end   = start + 12;
+
+    if (page===0) {
+        prev_btn.setAttribute('disabled', 'disabled');
+    }
+    else {
+        prev_btn.removeAttribute('disabled');
+        clear_btn.removeAttribute('disabled');
+    }
+    if (page==pageMax) {
+        next_btn.setAttribute('disabled', 'disabled');
+    }
+    else {
+        next_btn.removeAttribute('disabled');
+    }
+
+    while (patterns_div.hasChildNodes()) {
+        patterns_div.removeChild(patterns_div.firstChild);
+    }
+
+    for (var i = start, x; i < end; i++) {
+        file = patterns[i];
+        x = i%12;
+        pattern = document.createElement('div');
+        pattern.className = 'pattern';
+        if (selectedName===file.name) {
+            pattern.className += ' selected';
+            selected = pattern;
+        }
+        pattern.style.backgroundImage = 'url(' + raw_url + file.name + ')';
+        pattern.style.top = ((x - x%4) / 4) * 65 + 'px';
+        pattern.style.left = (x%4) * 65 + 'px';
+        pattern.setAttribute('data-name', file.name);
+        pattern.onmouseover = function() {
+            name_div.innerText = this.getAttribute('data-name');
+        };
+        pattern.onmouseout = function() {
+            name_div.innerText = (selected)?selected.getAttribute('data-name'):'';
+        };
+        pattern.onclick = function() {
+            if (selected) {
+                selected.className = 'pattern';
+            }
+            this.className = 'pattern selected';
+            selected = this;
+            selectedName = this.getAttribute('data-name');
+            chrome.extension.sendMessage({
+                pattern: selectedName, 
+                selector: target_div.value,
+            });
+        };
+        patterns_div.appendChild(pattern);
+    }
+}
+
+prev_btn.onclick = function() {
+    page--;
+    loadPatterns();
+    chrome.extension.sendMessage({page: page});    
+};
+next_btn.onclick = function() {
+    page++;
+    loadPatterns();
+    chrome.extension.sendMessage({page: page});    
+};
+
+clear_btn.onclick = function() {
+  page=0;
+  chrome.extension.sendMessage({clear:true, selector: target_div.value});
+};
+
+chrome.extension.sendMessage('getPatterns', function(p) {
+    patterns     = p.patterns;
+    selectedName = p.selected;
+    page         = p.page;
+    pageMax      = Math.floor(patterns.length / 12);
+    if (p.selector) {
+        target_div.value = p.selector;        
+    }
+
+    loadPatterns();
+});
